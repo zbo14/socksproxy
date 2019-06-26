77f3a0a34899a046ab3f153298ac7f7fd76c5549
zbo14 <zbalder@starry.com>
2019-06-26 15:56:08 -0400

    add server alive interval

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ ssh \
   -D 0.0.0.0:$LOCAL_PORT \
   -i /home/socksproxy/.ssh/id_ed25519 \
   -o IdentitiesOnly=yes \
+  -o ServerAliveInterval=15 \
   -o StrictHostKeyChecking=yes \
   -p $REMOTE_PORT \
   socksproxy@$HOST
