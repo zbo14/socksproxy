5bf731649941d8486a0c6ab10436bedc6b45597f
zbo14 <zbalder@starry.com>
2019-09-24 11:45:19 -0400

    change ServerAliveInterval

--- socksproxy.sh
+++ socksproxy.sh
@@ -5,8 +5,7 @@ autossh \
   -D 127.0.0.1:$LOCAL_PORT \
   -i /root/.ssh/socksproxy \
   -o IdentitiesOnly=yes \
-  -o ServerAliveCountMax=3 \
-  -o ServerAliveInterval=30 \
+  -o ServerAliveInterval=20 \
   -o StrictHostKeyChecking=no \
   -p $REMOTE_PORT \
   socksproxy@$HOST
