ef2772330e42859fbf98890dec44ea4c9ff2419b
Zach Balder <zbo14@users.noreply.github.com>
2019-09-26 14:28:22 -0400

    Revert "Merge develop"

--- socksproxy.sh
+++ socksproxy.sh
@@ -5,7 +5,8 @@ autossh \
   -D 127.0.0.1:$LOCAL_PORT \
   -i /root/.ssh/socksproxy \
   -o IdentitiesOnly=yes \
-  -o ServerAliveInterval=20 \
+  -o ServerAliveCountMax=3 \
+  -o ServerAliveInterval=30 \
   -o StrictHostKeyChecking=no \
   -p $REMOTE_PORT \
   socksproxy@$HOST
